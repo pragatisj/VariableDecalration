294330e337b98798cb0c13a64d757619c69b94fd
pragatisj <pragatijadhav2121@gmail.com>
2023-07-02 14:45:10 +0530

    Practice Q 5

---
+++ VariableDeclaration.java
@@ -0,0 +1,17 @@
+
+    public class VariableDeclaration {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int age = 25;
+        double height = 1.75;
+        String name = "John Doe";
+
+        // Variable usage
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+    }
+}
+
+    
+
